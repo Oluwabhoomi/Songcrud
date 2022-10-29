9e2f731b8c67a33c1565fe77aebca03dcff5c2e8
Oluwabhoomi <ayoluwabunmi@gmail.com>
2022-10-29 20:36:14 +0100

    initial commit week5

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,20 +1,19 @@
-from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
 class Artiste(models.Model):
-    first_name = models.Charfield(max_length=225)
-    last_name = models.Charfield(max_length=225)
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
     age = models.IntegerField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 class Song(models.Model):
-    title = models.Charfield(max_length=225)
-    date_released = models.Datefield()
-    likes = models.Integerfield()
+    title = models.CharField(max_length=225)
+    date_released = models.DateField()
+    likes = models.IntegerField()
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
 
     def __str__(self):
