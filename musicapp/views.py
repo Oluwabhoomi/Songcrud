e4a1a93047793ea41740fd93a0661aabba618975
Oluwabhoomi <ayoluwabunmi@gmail.com>
2022-11-03 23:30:04 +0100

    I commit django restframework

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
+
+from .models import Song, Artiste
+from .serializers import SongSerializer, ArtisteSerializer
+
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class ArtisteList(generics.ListCreateAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
+
+class ArtisteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
+    
 
 
 # Create your views here.
