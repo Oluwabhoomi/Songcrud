4f860abc8b52176be5bd9e79980ba86f563909ce
Oluwabhoomi <ayoluwabunmi@gmail.com>
2022-11-04 14:24:04 +0100

    initial commit for week6

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import SongList, SongDetail, ArtisteList, ArtisteDetail
 
 urlpatterns = [
-    path("<int:pk>/", SongDetail.as_view(), name="song_detail"),
-    path("", SongList.as_view(), name="song_list"),
-    path("<int:pk>/", ArtisteDetail.as_view(), name="artiste_detail"),
-    path("", ArtisteList.as_view(), name="artiste_list"),
+    path("song/<int:pk>/", SongDetail.as_view(), name="song_detail"),
+    path("song/", SongList.as_view(), name="song_list"),
+    path("artiste/<int:pk>/", ArtisteDetail.as_view(), name="artiste_detail"),
+    path("artiste/", ArtisteList.as_view(), name="artiste_list"),
 ]
     
--- songcrud/urls.py
+++ songcrud/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/song", include("musicapp.urls")),
-    path("api/artiste", include("musicapp.urls")),
+    path("api/song/", include("musicapp.urls")),
+    path("api/artiste/", include("musicapp.urls")),
+    path('api/', include('musicapp.urls')),
 ]
